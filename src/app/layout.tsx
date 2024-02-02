a11497e31a47bc94a563f03e953de02c3f1ff5a2
Max S <dragoonz@yandex.ru>
2024-02-02 16:35:22 +0300

    POSHEL PO KOMNATE STYLEK, STYLEK...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -15,7 +14,6 @@ const NAV_MENU = [
     href: '/github',
   },
 ];
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -29,29 +27,66 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="font-roboto">
         <header className="max-w-[1740px] px-8 py-4 mx-auto">
-          <div className="flex items-center justify-between">
+          <div className="flex items-start justify-between">
             <a href="/">
               <div className="flex items-center cursor-pointer">
-                <Image src="" alt="supalogo" width="64" height="66" />
-                <span className="text-3xl font-bold ml-4">Blast Frontend</span>
+                <Image
+                  src="/logo.png"
+                  alt="supablast"
+                  width="160"
+                  height="160"
+                />
               </div>
             </a>
-            <nav className="flex items-center">
+            <nav className="flex items-center font-sora pt-8">
               <ul className="flex items-center">
                 {NAV_MENU.map((menu) => (
-                  <li key={menu.title} className="mr-8 hover:text-[#F85150]">
+                  <li
+                    key={menu.title}
+                    className="mr-8 hover:text-[#F85150] text-md"
+                  >
                     <Link href={menu.href}>{menu.title}</Link>
                   </li>
                 ))}
               </ul>
-              <button
-                className={
-                  'btn bg-[#F85150] px-4 py-2 rounded-lg text-white font-bold uppercase'
-                }
-              >
-                {'Connect wallet'}
+              <button className="flex items-center border rounded-md px-4 py-2 border-[#e96828] space-x-2 cta">
+                <span className="">Connect wallet_</span>
+                <span className="">
+                  <svg
+                    width="48px"
+                    height="24px"
+                    viewBox="0 0 66 43"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                  >
+                    <g
+                      id="arrow"
+                      stroke="none"
+                      strokeWidth="1"
+                      fill="none"
+                      fillRule="evenodd"
+                    >
+                      <path
+                        className="one"
+                        d="M40.1543933,3.89485454 L43.9763149,0.139296592 C44.1708311,-0.0518420739 44.4826329,-0.0518571125 44.6771675,0.139262789 L65.6916134,20.7848311 C66.0855801,21.1718824 66.0911863,21.8050225 65.704135,22.1989893 C65.7000188,22.2031791 65.6958657,22.2073326 65.6916762,22.2114492 L44.677098,42.8607841 C44.4825957,43.0519059 44.1708242,43.0519358 43.9762853,42.8608513 L40.1545186,39.1069479 C39.9575152,38.9134427 39.9546793,38.5968729 40.1481845,38.3998695 C40.1502893,38.3977268 40.1524132,38.395603 40.1545562,38.3934985 L56.9937789,21.8567812 C57.1908028,21.6632968 57.193672,21.3467273 57.0001876,21.1497035 C56.9980647,21.1475418 56.9959223,21.1453995 56.9937605,21.1432767 L40.1545208,4.60825197 C39.9574869,4.41477773 39.9546013,4.09820839 40.1480756,3.90117456 C40.1501626,3.89904911 40.1522686,3.89694235 40.1543933,3.89485454 Z"
+                        fill="#0c0a09"
+                      ></path>
+                      <path
+                        className="two"
+                        d="M20.1543933,3.89485454 L23.9763149,0.139296592 C24.1708311,-0.0518420739 24.4826329,-0.0518571125 24.6771675,0.139262789 L45.6916134,20.7848311 C46.0855801,21.1718824 46.0911863,21.8050225 45.704135,22.1989893 C45.7000188,22.2031791 45.6958657,22.2073326 45.6916762,22.2114492 L24.677098,42.8607841 C24.4825957,43.0519059 24.1708242,43.0519358 23.9762853,42.8608513 L20.1545186,39.1069479 C19.9575152,38.9134427 19.9546793,38.5968729 20.1481845,38.3998695 C20.1502893,38.3977268 20.1524132,38.395603 20.1545562,38.3934985 L36.9937789,21.8567812 C37.1908028,21.6632968 37.193672,21.3467273 37.0001876,21.1497035 C36.9980647,21.1475418 36.9959223,21.1453995 36.9937605,21.1432767 L20.1545208,4.60825197 C19.9574869,4.41477773 19.9546013,4.09820839 20.1480756,3.90117456 C20.1501626,3.89904911 20.1522686,3.89694235 20.1543933,3.89485454 Z"
+                        fill="#0c0a09"
+                      ></path>
+                      <path
+                        className="three"
+                        d="M0.154393339,3.89485454 L3.97631488,0.139296592 C4.17083111,-0.0518420739 4.48263286,-0.0518571125 4.67716753,0.139262789 L25.6916134,20.7848311 C26.0855801,21.1718824 26.0911863,21.8050225 25.704135,22.1989893 C25.7000188,22.2031791 25.6958657,22.2073326 25.6916762,22.2114492 L4.67709797,42.8607841 C4.48259567,43.0519059 4.17082418,43.0519358 3.97628526,42.8608513 L0.154518591,39.1069479 C-0.0424848215,38.9134427 -0.0453206733,38.5968729 0.148184538,38.3998695 C0.150289256,38.3977268 0.152413239,38.395603 0.154556228,38.3934985 L16.9937789,21.8567812 C17.1908028,21.6632968 17.193672,21.3467273 17.0001876,21.1497035 C16.9980647,21.1475418 16.9959223,21.1453995 16.9937605,21.1432767 L0.15452076,4.60825197 C-0.0425130651,4.41477773 -0.0453986756,4.09820839 0.148075568,3.90117456 C0.150162624,3.89904911 0.152268631,3.89694235 0.154393339,3.89485454 Z"
+                        fill="#0c0a09"
+                      ></path>
+                    </g>
+                  </svg>
+                </span>
               </button>
             </nav>
           </div>
@@ -59,6 +94,14 @@ export default function RootLayout({
         <main className="flex min-h-screen flex-col items-center justify-between px-24 py-12">
           {children}
         </main>
+        <footer>
+          <div className=" fixed bottom-0 left-auto right-12">
+            <p className="font-digital flex justify-center">
+              {'>'} GIVE ME YOUR WETH
+            </p>
+            <Image src="/mine-footer.png" alt="" width={256} height={256} />
+          </div>
+        </footer>
       </body>
     </html>
   );
