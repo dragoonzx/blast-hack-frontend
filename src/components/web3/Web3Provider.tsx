03e12b19613647db50303394ad9c9dfdaf556e76
Tointer <35668369+Tointer@users.noreply.github.com>
2024-02-04 08:51:17 +0300

    use config file in Web3Provider

--- src/components/web3/Web3Provider.tsx
+++ src/components/web3/Web3Provider.tsx
@@ -4,27 +4,7 @@ import { WagmiProvider, createConfig, http } from 'wagmi';
 import { blastSepolia } from 'wagmi/chains';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ConnectKitProvider, getDefaultConfig } from 'connectkit';
-
-const config = createConfig(
-  getDefaultConfig({
-    // Your dApps chains
-    chains: [blastSepolia],
-    transports: {
-      [blastSepolia.id]: http(blastSepolia.rpcUrls.default.http[0]),
-    },
-
-    // Required API Keys
-    walletConnectProjectId: '',
-
-    // Required App Info
-    appName: 'Your App Name',
-
-    // Optional App Info
-    appDescription: 'MineBlast',
-    appUrl: 'https://mineblast.co', // your app's url
-    appIcon: 'https://mineblast.co/logo.png', // your app's icon, no bigger than 1024x1024px (max. 1MB)
-  })
-);
+import { config } from '../../lib/wagmiConfig';
 
 const queryClient = new QueryClient();
 
