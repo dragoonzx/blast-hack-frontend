ffe9dd7f150b4e27f3190bb9ef0ff994d8c41f56
Tointer <35668369+Tointer@users.noreply.github.com>
2024-02-13 12:52:47 +0300

    create token al view

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -25,30 +25,89 @@ const CreatePage = () => {
   const { address, isConnecting, isDisconnected } = useAccount();
   const ETHbalance = useBalance({ address });
   
-
-  const [previewVaultData, setPreviewVaultData] = useState<MineblastProjectData>({
-    vaultAddress: '0x0',
-    pairAddress: '0x0',
-    tokenAddress: '0x0',
-    tokenName: '...',
-    tokenSymbol: '...',
-    tokenTotalSupply: 0,
-    tokenPriceInUSD: 0,
-    projectOutputPerSecond: 0,
-    projectEndDate: new Date(Number(10000000000)),
-    TVLInUSD: 0,
-    pairETHBalanceRaw: 0n,
-    pairTokenBalanceRaw: 0n,
-  });
-  //<VaultInfo projectData={previewVaultData} ETHPrice={2660} className='opacity-15 mt-4'/>
+  const [tokenName, setTokenName] = useState<string>('');
+  const [tokenSymbol, setTokenSymbol] = useState<string>('');
+  const [totalSupply, setTotalSupply] = useState<number|undefined>(0);
+  const [ownerShare, setOwnerShare] = useState<number|undefined>(0);
+  const [endDate, setEndDate] = useState<Date|undefined>();
 
   return (
     <div className="flex items-start justify-center w-full space-x-8">
       <div className="w-[435px] flex flex-col">
-        <NewTokenForm onChange={(s) => {}}/>
-        <div className="flex justify-end w-full">
-        <Button className="mt-4">Create</Button>
-        </div>
+      <Card className='h-[200px] w-[400px]'>
+          <CardHeader>
+              <h3 className="font-semibold leading-none tracking-tight">Create token & vault</h3>
+          </CardHeader>
+          <CardContent>
+          <div className="font-sora">
+                <div className='flex flex-col'>
+                <label className='text-xs'>token name</label>
+                <div className='flex items-center'>
+                    <MineblastInput
+                    className=''
+                    type="text" 
+                    placeholder="Token Name"
+                    value={tokenName}
+                    onChange={setTokenName}
+                    />
+                </div>
+                </div>
+                <div className='flex flex-col'>
+                <label className='text-xs'>symbol</label>
+                <div className='flex items-center'>
+                    <p className='text-xl'>$ </p>
+                    <MineblastInput
+                    type="text" 
+                    placeholder="TKN"
+                    value={tokenSymbol}
+                    onChange={setTokenSymbol}
+                    />
+                </div>
+                </div>
+            </div>
+          </CardContent>
+      </Card>
+      <Card className='h-[200px] w-[400px] absolute translate-x-[200px] translate-y-[130px] bg-gray-900'>
+          <CardHeader>
+          </CardHeader>
+          <CardContent>
+            <div className="font-sora">
+                <div className='flex flex-col'>
+                  <label className='text-xs'>total supply</label>
+                  <div className='flex items-center'>
+                      <NumericInput
+                      className=''
+                      placeholder="100 000 000"
+                      value={totalSupply}
+                      onChange={setTotalSupply}
+                      />
+                  </div>
+                </div>
+                <div className='flex flex-col'>
+                  <label className='text-xs'>creator share %</label>
+                  <div className='flex items-center'>
+                      <NumericInput
+                      placeholder="9.5"
+                      value={ownerShare}
+                      onChange={setOwnerShare}
+                      />
+                  </div>
+                </div>
+            </div>
+          </CardContent>
+      </Card>
+      <Card className='h-[110px] w-[400px] absolute -translate-x-[220px] translate-y-[220px] bg-gray-900'>
+          <CardContent>
+            <div className='mt-3'>
+            <label className='text-xs'>end date</label>
+            <DatePickerWithPresets value={endDate} onChange={setEndDate}/>
+            </div>
+          </CardContent>
+      </Card>
+      <div className="">
+      <Button className="absolute translate-x-[200px] translate-y-[150px]">Create</Button>
+      </div>
+
       </div>
     </div>
   );
